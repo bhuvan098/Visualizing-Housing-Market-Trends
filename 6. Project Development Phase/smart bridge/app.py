a747717656b2cd5b8bcc76d2d3aeae70f7e33072
Bestha Bhuvan <bhuvanbestha2004@gmail.com>
2026-02-17 13:33:43 +0530

    Add files via upload

---
+++ 6. Project Development Phase/smart bridge/app.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 13 20:07:07 2026
+
+@author: bhuva
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=False)
